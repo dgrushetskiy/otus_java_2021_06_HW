8fc60079924fe5fe5a1d1437e6ef54cfdbbf0104
dgrushetskiy <dgrushetskiy@gmail.com>
2021-09-05 20:01:49 +0300

    main update hw01-03

---
+++ hw03-qa/src/main/java/ru/otus/testing/example/Start.java
@@ -0,0 +1,7 @@
+package ru.otus.testing.example;
+
+public class Start {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ hw03-qa/src/main/java/ru/otus/testing/example/service/IOService.java
@@ -0,0 +1,7 @@
+package ru.otus.testing.example.service;
+
+public interface IOService {
+    void out(String message);
+
+    String readString();
+}
