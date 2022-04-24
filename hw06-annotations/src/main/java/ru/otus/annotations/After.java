f0718aa02bd64d7a44e8c9e0f9df9b02eebdece5
dgrushetskiy <dgrushetskiy@gmail.com>
2022-04-24 04:46:47 +0300

    HW 06 Annotations add annotation

--- hw06-annotations/src/main/java/ru/otus/annotations/After.java
+++ hw06-annotations/src/main/java/ru/otus/annotations/After.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE_USE})
-public @interface Email {
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface After {
 }
--- hw06-annotations/src/main/java/ru/otus/annotations/Test.java
+++ hw06-annotations/src/main/java/ru/otus/annotations/Test.java
@@ -1,15 +1,11 @@
 package ru.otus.annotations;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE_USE})
-@Email
-@Immutable
-@Inherited
-public @interface ImmutableEmail {
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Test {
 }
