79e22f2c5c7832c72155c660d2eb17869fa0d2c4
dgrushetskiy <dgrushetskiy@gmail.com>
2022-06-08 00:04:30 +0300

    HW 08 GC add optimization

--- hw08-gc/src/main/java/ru/calculator/Summator.java
+++ hw08-gc/src/main/java/ru/calculator/Summator.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Summator {
-    private Integer sum = 0;
-    private Integer prevValue = 0;
-    private Integer prevPrevValue = 0;
-    private Integer sumLastThreeValues = 0;
-    private Integer someValue = 0;
+    private int sum = 0;
+    private int prevValue = 0;
+    private int prevPrevValue = 0;
+    private int sumLastThreeValues = 0;
+    private int someValue = 0;
     private final List<Data> listValues = new ArrayList<>();
 
     //!!! сигнатуру метода менять нельзя
     public void calc(Data data) {
         listValues.add(data);
-        if (listValues.size() % 6_600_000 == 0) {
+        if (listValues.size() % 5_700_000 == 0) {
             listValues.clear();
         }
         sum += data.getValue();
